f08ab9f489ed175febaf518eb2c3bbd852826d3e
SKY2422 <tkrhddbf@gmail.com>
2022-08-16 10:41:03 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommonJS
+
+// import
+// const autoprefixer = require('autoprefixer')
+
+// export {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
